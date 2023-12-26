3fbb64aa2854dea7fb57dbefeb4ececd4df43fe4
Nikita Gurin <gurinnickita@gmail.com>
2023-12-27 00:31:51 +0300

    add init

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "std.h"
+
+int main() {
+    int num1, num2;
+    cout << "Input first number: " << endl;
+    cin >> num1;
+    cout << "Input second number: " << endl;
+    cin >> num2;
+    return 0;
+}
